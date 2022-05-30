8fd955ff18bfff79779dac5848dd3935fa9251a7
t-mahito <qqna42p9@gmail.com>
2022-05-30 16:39:01 +0900

    バリデーションの設定とモデル同士の関連付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+validates :name,         presence: true
+validates :text,         presence: true
+validates :category_id,  presence: true
+validates :condition_id, presence: true
+validates :charge_id,    presence: true
+validates :area_id,      presence: true
+validates :deliverly_id, presence: true
+validates :price,        presence: true
+
+belongs_to :user
 end
