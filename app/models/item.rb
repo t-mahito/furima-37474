8c547aabc755f467b95be1e5b57e64ff21600e0d
t-mahito <qqna42p9@gmail.com>
2022-06-01 16:34:14 +0900

    condition　バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 validates :name,         presence: true
 validates :text,         presence: true
 validates :category_id,  numericality: {other_than: 1 , message: "can't be blank"}
-validates :condition_id, presence: true
+validates :condition_id, numericality: {other_than: 1 , message: "can't be blank"}
 validates :charge_id,    presence: true
 validates :area_id,      presence: true
 validates :deliverly_id, presence: true
