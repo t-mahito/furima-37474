10c0922ca31988bfdd2e327f02fa4f348a4478b2
t-mahito <qqna42p9@gmail.com>
2022-06-01 16:49:31 +0900

    Active hash area記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :charge
+  belongs_to :area
 
 
 
@@ -11,7 +12,7 @@ validates :text,         presence: true
 validates :category_id,  numericality: {other_than: 1 , message: "can't be blank"}
 validates :condition_id, numericality: {other_than: 1 , message: "can't be blank"}
 validates :charge_id,    numericality: {other_than: 1 , message: "can't be blank"}
-validates :area_id,      presence: true
+validates :area_id,      numericality: {other_than: 1 , message: "can't be blank"}
 validates :deliverly_id, presence: true
 validates :price,        presence: true
 validates :image,        presence: true
