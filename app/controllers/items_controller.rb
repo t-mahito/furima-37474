6ca0fdf7672aad6937d80814c61b393c0f5f30b1
t-mahito <qqna42p9@gmail.com>
2022-06-07 20:53:27 +0900

    コントローラーの可読性向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user! , only:[:new,:create,:edit] 
+  before_action :find_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -21,11 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user.id == current_user.id
      redirect_to action: :index
     end
@@ -33,7 +32,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -48,4 +46,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name,:text,:category_id,:condition_id,:charge_id,:area_id,:delivery_id,:price,:image).merge(user_id: current_user.id)
    end
 
+   def find_item
+    @item = Item.find(params[:id])
+   end
 end
