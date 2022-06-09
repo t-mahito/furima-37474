730bda13e3171a51977b2fbe10b6d74fe5a8aa73
t-mahito <qqna42p9@gmail.com>
2022-06-09 18:12:42 +0900

    adressモデルのmigrate記述

--- db/migrate/20220609090642_create_addresses.rb
+++ db/migrate/20220609090642_create_addresses.rb
@@ -3,6 +3,13 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
 
       t.timestamps
+      t.string       :postal_code,   null: false
+      t.integer      :area_id,       null: false
+      t.string       :city,          null: false
+      t.string       :address,       null: false
+      t.string       :building_name
+      t.string       :phone_number,  null: false
+      t.references   :order,         null: false, foreign_key: true
     end
   end
 end
