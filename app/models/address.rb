f4b3f5d2d11b379a73c3d98601ed0b9335939408
t-mahito <qqna42p9@gmail.com>
2022-06-09 18:19:39 +0900

    addressモデルへの記述　アソシエーション含む

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,13 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area_id
+
+  validates :postal_code,  presence: true
+  validates :area_id,      numericality: {other_than: 1 , message: "can't be blank"}
+  validates :city,         presence: true
+  validates :address,      presence: true
+  validates :phone_number, presence: true
+
+  belongs_to: order
+
 end
