908a2805ae8f1c7af4fe5a34abc981d05e26ebc5
t-mahito <qqna42p9@gmail.com>
2022-06-16 11:49:04 +0900

    重複したバリデーションの削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,7 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
-
-  validates :postal_code,  presence: true
-  validates :area_id,      numericality: {other_than: 1 , message: "can't be blank"}
-  validates :city,         presence: true
-  validates :address,      presence: true
-  validates :phone_number, presence: true
-
+  
   belongs_to :order
 
 end
